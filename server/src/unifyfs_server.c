0a84f7e619d2d92f721d4984ff3672e0464312ad
Adam Moody <moody20@llnl.gov>
2020-01-11 22:55:15 -0800

    track file extents in server with segment tree

--- server/src/unifyfs_server.c
+++ server/src/unifyfs_server.c
@@ -55,6 +55,8 @@ size_t glb_host_ndx;        // index of localhost in glb_servers
 size_t glb_num_servers;     // size of glb_servers array
 server_info_t* glb_servers; // array of server_info_t
 
+struct gfid2ext_tree glb_gfid2ext;
+
 unifyfs_cfg_t server_cfg;
 
 static ABT_mutex app_configs_abt_sync;
@@ -377,6 +379,9 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    /* initialize our tree that maps a gfid to its extent tree */
+    gfid2ext_tree_init(&glb_gfid2ext);
+
     LOGDBG("finished service initialization");
 
     rc = unifyfs_publish_server_pids();
@@ -393,6 +398,9 @@ int main(int argc, char* argv[])
             break;
         }
     }
+
+    /* tear down gfid-to-extents tree */
+    gfid2ext_tree_destroy(&glb_gfid2ext);
 
     LOGDBG("stopping service manager thread");
     rc = svcmgr_fini();
